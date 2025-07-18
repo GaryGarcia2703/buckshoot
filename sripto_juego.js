59349c0cce63746fa40e3c8fe0c60d144bb5b8a8
GaryGarcia2703 <Gary.Garcia2703@gmail.com>
2025-07-18 15:17:27 -0300

    bsr_alpha_update

--- sripto_juego.js
+++ sripto_juego.js
@@ -1,40 +1,60 @@
 let pistola = document.querySelector('p#pistola')
 let res = document.querySelector('div#res')
+let repeatbtn = document.createElement('button')
+repeatbtn.innerText = 'Proximo Round?'
 let balas = []
 let roja = 'Buckshoot'
 let azul = 'Blank'
-
-for (i = 1; i <= 3; i++) {
-    balas.push(roja)
-}
-for (i = 1; i <=3; i++) {
-    balas.push(azul)
-}
 
 function shuffle(array) {
     for(let i = array.length -1; i > 0; i--) {
         const random = Math.floor(Math.random() * (i + 1))
 
-        [array[i], array[random]] = [array[random], array[i]]
+        let temp = array[i]
+        array[i] = array[random]
+        array[random] = temp
     }
 }
-shuffle(balas)
+
+function repeat() {
+    balas = []
+    for (let i = 1; i <= 3; i++) {
+        balas.push(roja)
+    }
+    for (let i = 1; i <=3; i++) {
+        balas.push(azul)
+    }
+    shuffle(balas)
+    console.log(`balas embaralhadas ${balas}`)
+
+    res.innerHTML = "Novo round iniciado";
+    res.style.backgroundColor = "#161316";
+
+
+    if (res.contains(repeatbtn)) {
+        res.removeChild(repeatbtn);
+    }
+
+}
 
 function resultado() {
      if (balas.length === 0) {
-        res.innerHTML = "Acabaram as balas";
-        res.style.backgroundColor = "gray";
-        return;
+        res.innerHTML = "Acabaram as balas"
+        res.style.backgroundColor = "gray"
+        res.appendChild(repeatbtn)
+        return
     }
 
     if (balas[0] === roja) {
-        res.innerHTML = "tiro"
+        res.innerHTML = "💥 Tiro!"
         res.style.backgroundColor = "red"
 
     } else {
-        res.innerHTML = "vazia"
+        res.innerHTML = "🔵 Vazia"
         res.style.backgroundColor = "blue"
     }
     
     balas.shift();
 }
+repeatbtn.addEventListener('click' , repeat)
+repeat()
